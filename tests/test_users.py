b09ff95d1dc3c14aab2440dce6a3f0ebea8a1e2d
vitaliykalachev <vitaliy.kalachev@gmail.com>
2022-10-20 18:48:09 +0700

    Tests. Fixture scope

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,11 +1,24 @@
-from fastapi.testclient import TestClient
-from app.main import app
+from app import schemas
+from .database import client, session
 
 
-client = TestClient(app)
 
-def test_root():
+def test_root(client):
+    
     res = client.get("/")
     print(res.json().get('Hello'))
     assert res.json().get('Hello') == "Привет, как дела, заходи в гости"
+    assert res.status_code == 200
+    
+def test_create_user(client):
+    res = client.post("/users/", json={"email": "hello123@gmail.com", "password": "password123"})
+    print(res.json())
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "hello123@gmail.com"
+    assert res.status_code == 201
+    
+def test_login_user(client):
+    res = client.post(
+        "/login", data={"username": "hello123@gmail.com", "password": "password123"})
+    print(res.json())
     assert res.status_code == 200
