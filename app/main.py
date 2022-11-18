1096727c87af4352d4f7b6868651bdec1b90be2a
vitaliykalachev <vitaliy.kalachev@gmail.com>
2022-11-18 13:36:30 +0700

    Delete templates

--- app/main.py
+++ app/main.py
@@ -6,11 +6,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
-from .routers import post, user, auth, vote, template
-
-from .config import settings
-from pydantic import BaseModel
-from typing import Union
+from .routers import post, user, auth, vote
 
 from . import schemas
 from sqlalchemy.orm import Session
@@ -41,7 +37,7 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
-# app.include_router(template.router)
+
 
 
 
@@ -53,36 +49,3 @@ def root(request: Request):
 
 
 
-
-
-
-
-
-
-
-# @app.get("/", response_class=HTMLResponse, response_model=schemas.UserOut)
-# def read_item(request: Request, user: str, db: Session = Depends(get_db)):
-#     return templates.TemplateResponse(
-#         "test.html", {"request": request, "name": user}
-#    )
-
-# from starlette.responses import HTMLResponse
-
-
-# async def app(scope, receive, send):
-#     assert scope['type'] == 'http'
-#     response = HTMLResponse('<html><body><h1>Всё Пока</h1></body></html>')
-#     await response(scope, receive, send)
-
-# @app.get("/sweets/{id}", response_class=HTMLResponse)
-# async def read_item(request: Request, id: str ):
-    
-#     return templates.TemplateResponse("candy.html", {"request": request, "id": id})
-
-# @app.get("/sweets/candy")
-# async def candy(request: Request):
-#     return templates.TemplateResponse("sweets/candy.html", {"request": request})
-
-# @app.get("/test")
-# async def test(request: Request):
-#  1   return templates.TemplateResponse("test.html", {"request": request})
